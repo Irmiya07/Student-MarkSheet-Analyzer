ffed46e2fd2985d1c23e7db33eaa4a9c7199df9e
Irmiya <147790437+Irmiya07@users.noreply.github.com>
2025-09-22 22:33:15 +0530

    updated port

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,12 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from flasgger import Swagger
 import pandas as pd
+import os
 
 app = Flask(__name__)
 CORS(app)
+swagger = Swagger(app)
 
 @app.route('/analyze', methods=['POST'])
 def analyze_data():
@@ -31,4 +34,5 @@ def analyze_data():
   except Exception as e:
     return jsonify({'error': str(e)}), 500
 if __name__ == '__main__':
-  app.run(debug=True)
+  port = int(os.environ.get("PORT", 5000))  # use Render's port or default 5000
+  app.run(host="0.0.0.0", port=port)
